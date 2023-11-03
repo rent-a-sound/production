10cc60232f290eecbc1924013e3760533c2275f1
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-11-03 21:56:40 +0100

    Added media

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import { BsLightningChargeFill } from "react-icons/bs";
+import partyBox310 from '../public/party-box-310.png';
 
 const Hero = () => {
   return (
-    <div name="container" className='w-full h-screen md:-mt-16 bg-neutral-800 flex flex-col md:flex-row items-center justify-around md:justify-between font-montserrat'>
+    <div name="container" className='w-full h-screen md:-mt-16 bg-neutral-800 flex flex-col md:flex-row items-center justify-between md:justify-between font-montserrat'>
         <div name="left-half" className='text-white m-8 md:m-16 flex flex-col items-start justify-around'>
             <div name="title" className='font-bold text-4xl md:text-6xl mb-3'>LOREM IPSUM</div>
             <div name="subtitle" className='font-thin md:text-xl mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus, neque in feugiat scelerisque, elit magna semper nibh, nec vulputate magna sapien tincidunt diam. Fusce ac dui et augue vehic</div>
@@ -11,10 +12,7 @@ const Hero = () => {
                 <div name="tip-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus, neque in feugiat scelerisque, elit magna semper nibh, nec vulputate magna sapien tincidunt diam. Fusce ac dui et augue vehice </div>
             </div>
         </div>
-        <div>
-            <div>LOREM</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus, neque in feugiat scelerisque, elit magna semper nibh, nec vulputate magna sapien tincidunt diam. Fusce ac dui et augue vehic</div>
-        </div>
+            <img className="h-1/2" src={partyBox310}></img>
     </div>
   )
 }
