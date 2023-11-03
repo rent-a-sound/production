379da2a18e00514767ad98382f25a96047d54aef
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-11-04 00:39:45 +0100

    Adjusted color accents

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,15 +12,15 @@ const Hero = () => {
             <div name="title" className='font-bold text-4xl md:text-6xl mb-3'>LOREM IPSUM</div>
             <div name="subtitle" className='font-thin md:text-xl mb-5'>{lorem}</div>
             <div name="tip-container" className='font-extralight bg-neutral-900 p-4 mb-5 text-sm rounded-lg text-neutral-500 flex flex-row items-center justify-between'>
-                <div name="tip-icon" className='text-3xl pr-4'><BsLightningChargeFill/></div>
+                <div name="tip-icon" className='text-3xl pr-4 text-purple-500'><BsLightningChargeFill/></div>
                 <div name="tip-text">{lorem}</div>
             </div>
         </div>
             <div name='city-container' className="w-full flex flex-col md:justify-around items-center text-white">
-                <div name='novi-sad' className="bg-neutral-900 w-72 h-24 m-5 rounded-3xl flex flex-col items-center justify-center">
+                <div name='novi-sad' className="bg-neutral-900 w-72 h-24 m-5 rounded-3xl flex flex-col items-center justify-center border-2 border-purple-500">
                    <div className="m-5 font-bold text-2xl flex flex-row items-center justify-between"><FaMapMarkerAlt className="text-red-600 mr-1"/>NOVI SAD<TbArrowBigRightFilled className="ml-5"/></div>
                 </div>
-                <div name='beograd' className="bg-neutral-900 w-72 h-24 m-5 rounded-3xl flex flex-col items-center justify-center">
+                <div name='beograd' className="bg-neutral-900 w-72 h-24 m-5 rounded-3xl flex flex-col items-center justify-center border-2 border-purple-500">
                    <div className="m-5 font-bold text-2xl flex flex-row items-center justify-between"><FaMapMarkerAlt className="text-red-600 mr-1"/>BEOGRAD<TbArrowBigRightFilled className="ml-5"/></div>
                 </div>
             </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,11 @@
 import { CgMusicSpeaker } from "react-icons/cg";
 
 const Nav = () => {
-  let Links = [
-    { name: "ABOUT", link: "/" },
-    { name: "ORDER", link: "/" },
-  ];
-
   return (
     <div className="w-full top-0 left-0 bg-transparent font-montserrat">
       <div className="w-full flex flex-col items-center font-montserrat">
         <div className="font-bold p-1.5 pr-2 text-2xl mx-5 my-2 flex items-center text-white">
-          <CgMusicSpeaker className="mr-2 text-3xl text-purple-400"/>
+          <CgMusicSpeaker className="mr-2 text-3xl text-purple-500"/>
           RENT A SOUND
         </div>
       </div>
