e455e9684e959f438d98a16a5caf7393b8f4a5dc
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-11-10 20:44:08 +0100

    Minor padding adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="bg-neutral-950 overflow-hidden">
+    <div className="bg-neutral-950 overflow-hidden p-5">
       <Nav />
       <Hero />
     </div>
