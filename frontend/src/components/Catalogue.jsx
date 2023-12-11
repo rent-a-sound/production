b05a48daa34de62e10fe5d12973ad893ed42fc95
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-12-11 17:48:17 +0100

    Experimental Date Picker

--- frontend/src/components/Catalogue.jsx
+++ frontend/src/components/Catalogue.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 const Catalogue = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [selected, setSelected] = React.useState();
 
   useEffect(() => {
-    // Define the Flask app URL where the data is served
     const apiUrl = "http://127.0.0.1:5000/data";
-
-    // Fetch data from the Flask app using Axios
     axios
       .get(apiUrl)
       .then((response) => {
@@ -22,17 +22,20 @@ const Catalogue = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Data Display</h1>
-      {error && <p>Error: {error}</p>}
-      {data && (
-        <div>
-          <p>ID: {data.id}</p>
-          <p>Name: {data.name}</p>
-          <p>Age: {data.age}</p>
-          <p>City: {data.city}</p>
-        </div>
-      )}
+    <div className="w-full min-h-screen bg-neutral-950 flex flex-col items-center justify-start">
+      <DayPicker
+        mode="single"
+        onSelect={setSelected}
+        className="text-neutral-200 bg-neutral-800 p-10 rounded-3xl font-montserrat"
+        styles={{
+          caption: {
+            backgroundColor: "RGB(29,29,29)",
+            padding: "0.5rem",
+            borderRadius: "1rem",
+            marginBottom: "1rem",
+          },
+        }}
+      />
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,68 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
-                'montserrat': ['Montserrat'],
-                'lato': ['Lato'],
-                'garamond': ['Garamond']
-            },
+        montserrat: ["Montserrat"],
+        lato: ["Lato"],
+        garamond: ["Garamond"],
+      },
       keyframes: {
         nilemation: {
-          '0%': {
-            transform: 'translateY(0)',
-            opacity: '1',
+          "0%": {
+            transform: "translateY(0)",
+            opacity: "1",
           },
-          '38%': {
-            transform: 'translateY(0)',
-            animationTimingFunction: 'ease-out',
-            opacity: '1',
+          "38%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "ease-out",
+            opacity: "1",
           },
-          '50%': {
-            transform: 'translateY(500px)',
-            opacity: '0',
+          "50%": {
+            transform: "translateY(500px)",
+            opacity: "0",
           },
-          '55%': {
-            transform: 'translateY(-700px)',
-            animationTimingFunction: 'ease-in',
+          "55%": {
+            transform: "translateY(-700px)",
+            animationTimingFunction: "ease-in",
           },
-          '72%': {
-            transform: 'translateY(0)',
-            animationTimingFunction: 'ease-out',
+          "72%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "ease-out",
           },
-          '81%': {
-            transform: 'translateY(-32px)',
-            animationTimingFunction: 'ease-in',
+          "81%": {
+            transform: "translateY(-32px)",
+            animationTimingFunction: "ease-in",
           },
-          '90%': {
-            transform: 'translateY(0)',
-            animationTimingFunction: 'ease-out',
+          "90%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "ease-out",
           },
-          '95%': {
-            transform: 'translateY(-12px)',
-            animationTimingFunction: 'ease-in',
+          "95%": {
+            transform: "translateY(-12px)",
+            animationTimingFunction: "ease-in",
           },
-          '100%': {
-            transform: 'translateY(0)',
-            animationTimingFunction: 'ease-out',
+          "100%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "ease-out",
           },
-      },
+        },
         spinle: {
-          '0%': { transform: 'rotateY(360deg)' },
-          '25%': { transform: 'rotateY(0deg)' },
-          '100%': { transform: 'rotateY(0deg)' },
-        }
+          "0%": { transform: "rotateY(360deg)" },
+          "25%": { transform: "rotateY(0deg)" },
+          "100%": { transform: "rotateY(0deg)" },
+        },
+      },
+      animation: {
+        nilemation1: "nilemation 2s ease-in-out",
+        nilemation2: "nilemation 2s 0.2s ease-in-out",
+        nilemation3: "nilemation 2s 0.3s ease-in-out",
+        nilemation4: "nilemation 2s 0.1s ease-in-out",
+        spinle1: "spinle 6s 1.5s linear infinite",
+        spinle2: "spinle 6s linear infinite",
+      },
     },
-    animation: {
-      nilemation1: 'nilemation 2s ease-in-out',
-      nilemation2: 'nilemation 2s 0.2s ease-in-out',
-      nilemation3: 'nilemation 2s 0.3s ease-in-out',
-      nilemation4: 'nilemation 2s 0.1s ease-in-out',
-      spinle1: 'spinle 6s 1.5s linear infinite',
-      spinle2: 'spinle 6s linear infinite',
-    }
   },
-},
   plugins: [],
 };
