82ec0ce1cc813171615a96f61da2ae332d8ed590
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-12-16 18:27:46 +0100

    Separate res and cancel, single date support

--- backend/bot.py
+++ backend/bot.py
@@ -11,37 +11,81 @@ DATA_PATH: Final = './config.json'
 
 async def rez(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        object_id, start_date, end_date, mode = update.message.text.lstrip('/rez ').split(';')
+        valueArray = update.message.text.lstrip('/rez ').split(';')
+        object_id = valueArray[0]
+        start_date = valueArray[1]
+        if len(valueArray) > 2:
+            end_date = valueArray[2]
+        else:
+            end_date = None
 
         with open(DATA_PATH, 'r') as file:
             json_data = json.load(file)
 
         start_date = datetime.strptime(start_date, '%d-%m-%Y')
-        end_date = datetime.strptime(end_date, '%d-%m-%Y')
 
-        date_list = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
-        date_list = [date.strftime('%d-%m-%Y') for date in date_list]
+        if end_date is not None:
+            end_date = datetime.strptime(end_date, '%d-%m-%Y')
+            date_list = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
+            date_list = [date.strftime('%d-%m-%Y') for date in date_list]
+        else:
+            date_list = [start_date.strftime('%d-%m-%Y')]
 
         for item in json_data:
             if item.get("id") == object_id:
-                if mode == "add":
-                    for date_string in date_list:
-                        if date_string not in item["unavailable"]:
-                            item["unavailable"].append(date_string)
-                    with open(DATA_PATH, 'w') as file:
-                        json.dump(json_data, file, indent=2)
+                for date_string in date_list:
+                    if date_string not in item["unavailable"]:
+                        item["unavailable"].append(date_string)
+                with open(DATA_PATH, 'w') as file:
+                    json.dump(json_data, file, indent=2)
+                if end_date is not None:
                     await context.bot.send_message(chat_id=update.message.chat_id, text=f"Speaker id:{object_id} reserved from {start_date.strftime('%d-%m-%Y')} to {end_date.strftime('%d-%m-%Y')}.")
-                elif mode == "remove":
-                    for date_string in date_list:
-                        if date_string in item["unavailable"]:
-                            item["unavailable"].remove(date_string)
-                    with open(DATA_PATH, 'w') as file:
-                        json.dump(json_data, file, indent=2)
+                else:
+                    await context.bot.send_message(chat_id=update.message.chat_id, text=f"Speaker id:{object_id} reserved for {start_date.strftime('%d-%m-%Y')}.")
+                return
+            
+        await context.bot.send_message(chat_id=update.message.chat_id, text=f"Speaker id:{object_id} not found.")
+    
+    except ValueError as ve:
+        await context.bot.send_message(chat_id=update.message.chat_id, text=f"Error parsing dates: {str(ve)}")
+    except Exception as e:
+        await context.bot.send_message(chat_id=update.message.chat_id, text=f"An error occurred: {str(e)}")
+
+async def otkazi(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        valueArray = update.message.text.lstrip('/otkazi ').split(';')
+        object_id = valueArray[0]
+        start_date = valueArray[1]
+        if len(valueArray) > 2:
+            end_date = valueArray[2]
+        else:
+            end_date = None
+
+        with open(DATA_PATH, 'r') as file:
+            json_data = json.load(file)
+
+        start_date = datetime.strptime(start_date, '%d-%m-%Y')
+
+        if end_date is not None:
+            end_date = datetime.strptime(end_date, '%d-%m-%Y')
+            date_list = [start_date + timedelta(days=x) for x in range((end_date-start_date).days + 1)]
+            date_list = [date.strftime('%d-%m-%Y') for date in date_list]
+        else:
+            date_list = [start_date.strftime('%d-%m-%Y')]
+
+        for item in json_data:
+            if item.get("id") == object_id:
+                for date_string in date_list:
+                    if date_string in item["unavailable"]:
+                        item["unavailable"].remove(date_string)
+                with open(DATA_PATH, 'w') as file:
+                    json.dump(json_data, file, indent=2)
+                if end_date is not None:
                     await context.bot.send_message(chat_id=update.message.chat_id, text=f"Speaker id:{object_id} unreserved from {start_date.strftime('%d-%m-%Y')} to {end_date.strftime('%d-%m-%Y')}.")
                 else:
-                    await context.bot.send_message(chat_id=update.message.chat_id, text="Invalid mode. Use 'add' or 'remove'.")
+                    await context.bot.send_message(chat_id=update.message.chat_id, text=f"Speaker id:{object_id} unreserved for {start_date.strftime('%d-%m-%Y')}.")
                 return
-
+        
         await context.bot.send_message(chat_id=update.message.chat_id, text=f"Speaker id:{object_id} not found.")
     
     except ValueError as ve:
@@ -167,6 +211,7 @@ if __name__ == '__main__':
 
     # Commands
     app.add_handler(CommandHandler('rez', rez, filters=filters.Chat(chat_id=int(CHAT_ID))))
+    app.add_handler(CommandHandler('otkazi', otkazi, filters=filters.Chat(chat_id=int(CHAT_ID))))
     app.add_handler(CommandHandler('remove', remove_speaker, filters=filters.Chat(chat_id=int(CHAT_ID))))
     app.add_handler(CommandHandler('add', add_speaker, filters=filters.Chat(chat_id=int(CHAT_ID))))
     app.add_handler(CommandHandler('clean', clean_dates, filters=filters.Chat(chat_id=int(CHAT_ID))))
