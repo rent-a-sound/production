3c2c7ba59b307b606c410e850ddd85e6dbf23107
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2024-02-15 23:31:47 +0100

    Updated cors

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from flask_cors import CORS
 from typing import Final
+import requests
 import json
 
 app = Flask(__name__)
 
-CORS(app, origins="*")
+CORS(app, origins="https://zvucnici.com")
 DATA_PATH: Final = '../backend/config.json'
 
 def load_config():
@@ -37,6 +38,20 @@ def get_single():
     else:
         return json.dumps({"error": "Item not found"}), 404
     
+
+@app.route('/order', methods=['POST'])
+def send_order():
+    url = "https://api.telegram.org/bot6461461088:AAGWeqWqbFZzLaRsWasiMsR8xNZ3bx2pmXM/sendMessage"
+    text = request.args.get('text')
+
+    payload = {
+    'chat_id': '5783597838',
+    'text': text
+    }
+
+    response = requests.post(url, data=payload)
+
+    return str(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
